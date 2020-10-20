b1d2a55f664b12dc45b1dca7c3edc7beb31dac75
GulinAlexey <gulin-2001@mail.ru>
2020-10-21 01:11:47 +0700

    Добавлены закрытые поля для класса работников

--- lab6prog_sharp/Program.cs
+++ lab6prog_sharp/Program.cs
@@ -1,11 +1,18 @@
 ﻿//Гулин А. Н., ПИ-92. Лаб. 6 "Первая программа на С#"
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace lab6prog_sharp
 {
+    class Worker //работник заповедника
+    {
+        private int num_tr; //номер трудовой книжки
+        private string name_surname; //имя и фамилия
+        private string dolzh; //должность
+        private int hours; //кол-во рабочих часов
+        private int zarpl; //зарплата в месяц в рублях
+        private int progools; //кол-во прогулов (в днях)
+    }
+
     class Program
     {
         static void Main(string[] args)
