07a9454005a0e5caceb8f91f11702717a87924d7
GulinAlexey <gulin-2001@mail.ru>
2020-10-20 20:24:48 +0700

    Начало. Создание проекта

---
+++ lab6prog_sharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lab6prog_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
